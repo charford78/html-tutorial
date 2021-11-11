c20848b82ba8c98b5eb5f6e27a1cffecac4fb831
Charlie Ford <charles.ford1978@gmail.com>
2021-11-11 16:24:33 -0500

    Made some more changes.

--- js/user-detail.js
+++ js/user-detail.js
@@ -1,21 +1,22 @@
-let user = {
-    id: 1, 
-    username: "cford", 
-    firstname: "Charles", 
-    lastname: "Ford", 
-    phone: "411", 
-    email: "barf@barf.com", 
-    isReviewer: true, 
-    isAdmin: true
-}
+let user = {}
 
 const remove = () => {
     console.log("Delete user ...");
 }
 
+const getUserByPk = () => {
+    let id = getUrlId();
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open("GET", `http://localhost:54800/api/users/${id}`, true);
+    xhr.onload = () => {
+        user = xhr.response;
+        loaded();
+    }
+    xhr.send();
+}
+
 const loaded = () => {
-    var url = getUrlId();
-    
     document.getElementById("xid").value = user.id;
     document.getElementById("xusername").value = user.username;
     document.getElementById("xfirstname").value = user.firstname;
